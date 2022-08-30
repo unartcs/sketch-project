7e4b37f46f2b8e070a03436960b9fb61a9a168a4
unartcs <unartcs@gmail.com>
2022-08-30 14:01:30 +0200

    Edited style.css index.html and app.js
    Revamped the entire website, made a colour picker, added random button and button toggles.

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const divContainer = document.querySelector('.grid-container');
-// const brushColor = document.querySelector('.color-button')
-color = 'red';
 const colorButton = document.querySelector('.color-button');
 const clearButton = document.querySelector('.clear-button');
-const eraserButton = document.querySelector('.eraser-button')
+const eraserButton = document.querySelector('.eraser-button');
+const rgbButton = document.querySelector('.rgb-button');
+// const ftbButton = document.querySelector('.ftb-button')
+let randomColor = false;
+let eraser = false;
+let ftb = false;
+let bgColor = colorButton.value;
+const getGridT = document.querySelectorAll('.grid-1')
+let colors = bgColor.split('')
+
 function makeDiv(size) {
     deleteDiv = document.querySelectorAll('.grid-1')
     deleteDiv.forEach(function (deleteDiv) {
@@ -30,7 +37,21 @@ function spreadDiv(x) {
     const getGrid = document.querySelectorAll('.grid-1')
     getGrid.forEach((getGrid) => {
         getGrid.addEventListener('mouseover', function (e) {
-            getGrid.style.backgroundColor = color;
+            if (randomColor == true && eraser == false) {
+                getGrid.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`
+            } else if (eraser == true) {
+                getGrid.style.backgroundColor = 'white';
+            } 
+            // else if (ftb == true) {
+            //     colors = (bgColor.match(/[A-Za-z0-9]{2}/g).map(function(v) { return parseInt(v, 16) }).join(","))
+            //     colors = colors.split(',')
+            //     for (let i = 0;i < 1; i++) {
+
+            //     }
+            // }
+            else {
+                getGrid.style.backgroundColor = bgColor;
+            }
         }
         )
     }
@@ -51,27 +72,61 @@ function chooseGrid() {
     })
 }
 
-colorButton.addEventListener('click', function () {
-    color = prompt('Choose a color!');
-    if (typeof color == String) {
-        return color;
-    }
+colorButton.addEventListener('input', function () {
+    bgColor = colorButton.value;
+    return bgColor;
 })
 
 clearButton.addEventListener('click', function () {
     const getGrid = document.querySelectorAll('.grid-1')
     getGrid.forEach((getGrid) => {
-            getGrid.style.backgroundColor = 'black';
-        }
-        )
+        getGrid.style.backgroundColor = 'white';
     }
+    )
+}
 )
 
+
+rgbButton.addEventListener('click', function () {
+    if (randomColor == false) {
+        rgbButton.style.boxShadow = 'inset 0 0 100px rgb(1, 231, 212)';
+        eraserButton.style.boxShadow = 'inset 0 0 0px #000000';
+        eraser = false;
+        randomColor = true;
+    }
+    else {
+        rgbButton.style.boxShadow = 'inset 0 0 0px #000000';
+        randomColor = false;
+    }
+})
+
+
 eraserButton.addEventListener('click', function () {
-    color = 'black';
+    if (eraser == false) {
+        eraserButton.style.boxShadow = 'inset 0 0 100px rgb(1, 231, 212)';
+        bgColor = 'white'
+        rgbButton.style.boxShadow = 'inset 0 0 0px #000000';
+        randomColor = false;
+        eraser = true;
+    }
+    else {
+        eraserButton.style.boxShadow = 'inset 0 0 0px #000000';
+        bgColor = colorButton.value;
+        eraser = false;
+    }
 });
+
+// ftbButton.addEventListener('click',function() {
+//     if (ftb == false) {
+//         ftb = true;
+//         ftbButton.style.boxShadow = 'inset 0 0 100px rgb(1, 231, 212)';
+//         console.log(colors)
+//     } else {
+//         ftbButton.style.boxShadow = 'inset 0 0 0px #000000';
+//     }
+// })
 
 
 chooseGrid();
 
-// makeDiv(16);
+makeDiv(16);
